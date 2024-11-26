39ee805c5974fc0ca63a63b9cbf76124b470e18d
Diogo <diogo.paulino.santos@hotmail.com>
2024-11-26 16:38:27 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAgqpnXNxZ8abif8ssQq_C2s998w7jZb68",
+    apiKey: "AIzaSyAgqpnXNxZ8abif8ssQq_",
     authDomain: "api-aula-d6451.firebaseapp.com",
-    projectId: "api-aula-d6451",
+    projectId: "api-aula-1",
     storageBucket: "api-aula-d6451.appspot.com",
-    messagingSenderId: "48648388488",
-    appId: "1:48648388488:web:dc73e0a1496a52cd4b843d",
+    messagingSenderId: "48648",
+    appId: "1:48648388488:web:dc73e0",
     measurementId: "G-CR3MDWMWJF"
   },
   production: false
